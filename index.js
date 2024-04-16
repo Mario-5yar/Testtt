5d690951523faf996b567966d611dbd6de182c1c
Mario-5yar <72055560+Mario-5yar@users.noreply.github.com>
2024-04-16 07:14:17 +0200

    index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ bot.on("spawn", () => {
     bot.setControlState("forward", true);
     setTimeout(() => {
       bot.setControlState("forward", false);
+      bot.chat('/tpa oldmarius')
     }, 500);
   }, 2000);
 });
